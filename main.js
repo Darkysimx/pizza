96343132a02814a0d8bcebec749060bc1956ca5c
Darkysimx <111804476+Darkysimx@users.noreply.github.com>
2023-07-30 15:16:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+menuListArray = ["Pizza Vegetariana",
+                 "pizza de calabresa",
+                "pizza de marguerita"   ];
+
+function getMenu(){
+var htmldata;
+}
+
+function addItem(){
+var htmldata;
+var item=document.getElementById("addItem").value;
+
+
+}
+
+function addTop(){
+
+}
